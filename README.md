aaa5e7bb6df5b759233bea8f3da0dbb879e93b52
BigStrad <bigstrad@bigs-mbp.attlocal.net>
2019-08-25 16:40:28 -0500

    README and script tweaks

--- README.md
+++ README.md
@@ -16,5 +16,9 @@ You will likely need to update your firewall rule to allow your port.
 ```sh
 $ sudo ufw allow xxxx/tcp
 ```
+
+## Process Manager
+On the server, you will probably want to run with a process manager like [PS2](https://www.npmjs.com/package/pm2)
+
 ## Credits
 Added Docker and shell external shell script logic. Modified from [DigitalOcean Communty]() - How to Use Node.js and Github Webhooks to Keep Remote Projects in Sync
--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 # cd into repository path
-#cd /home/appuser/projects/twittee
-cd /Users/bigstrad/Documents/Development/NodeJSApps/twitteeTEST
-#cd /repo
+ca /path/to/repo
 
 # bring down the server
 docker-compose down
@@ -9,9 +7,5 @@ docker-compose down
 # pull latest
 git pull
 
-# cp .env
-#cp /home/appuser/projects/twittee-build/twittee.env /home/appuser/projects/twittee/api/.env
-#cp /data/twittee.env /repo/api/.env
-
 # bring up the server
-docker-compose up -d #--build -d
+docker-compose up --build -d
