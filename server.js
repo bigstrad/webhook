e00c0a9025a86dafd61abb2626407e895d2a02d6
BigStrad <bigstrad@bigs-mbp.attlocal.net>
2019-08-27 22:22:29 -0500

    cleanup

--- server.js
+++ server.js
@@ -3,25 +3,28 @@ const crypto = require('crypto');
 const { exec } = require('child_process');
 const shell = require('shelljs');
 const path = require('path'); 
-// const dotenv = require('dotenv');
-// dotenv.config();
+// extra config required for PM2
 const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+// just a little obfuscation loop
+function swapChar4Asterisk(input) {
+    let out = '';
+    if(input===undefined || input==='') {
+        return out;
+    }
+    for (i = 0; i < input.length; i++) { 
+        out += '*';
+    }
+    return out;
+}
 
 // map environment variables to local variables
 const port = process.env.TWIT_TEE_WEBHOOK_PORT || 7001;
 const secret = process.env.TWIT_TEE_WEBHOOK_SECRET;
 const script = process.env.TWIT_TEE_WEBHOOK_SCRIPT;
 const branch = process.env.TWIT_TEE_WEBHOOK_BRANCH; 
-console.log("port => ", port);
-console.log("secret => ", secret);
-console.log("script => ", script);
-console.log("branch => ", branch);
 http.createServer(function (req, res) {
     req.on('data', function(chunk) {
-        console.log("port => ", port);
-        console.log("secret => ", secret);
-        console.log("script => ", script);
-        console.log("branch => ", branch);
         const json = JSON.parse(chunk);
         let sig= "sha1=" + crypto.createHmac('sha1', secret).update(JSON.stringify(json)).digest('hex');
         console.log("branch => ", json.ref);
@@ -35,4 +38,9 @@ http.createServer(function (req, res) {
     });
 
     res.end();
-}).listen(port);
+}).listen(port, function(){
+    console.log("port => ", port);
+    console.log("secret => ", swapChar4Asterisk(secret)); 
+    console.log("script => ", script);
+    console.log("branch => ", branch);
+   });
